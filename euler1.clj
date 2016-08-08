6a8278bcac04aa189df983c058036181cbed42bf
Marat Tukhvatullin <marat@cinarra.com>
2016-08-08 13:26:07 +0300

    Solutions

--- euler1.clj
+++
@@ -1,2 +0,0 @@
-(defn euler1 [] (reduce + (filter (fn [x] (or (= 0 (mod x 3)) (= 0 (mod x 5)))) (range 0 1000))))
-(euler1)
--- euler2.clj
+++
@@ -1,3 +0,0 @@
-
-(def fib (lazy-cat [1 1] (map + (rest fib) fib)))
-(defn euler2 [] (reduce + (filter even? (take-while (fn [x] (< x 4000000)) fib))))
